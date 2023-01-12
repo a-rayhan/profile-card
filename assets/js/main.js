fb2d948094c3311cdc9473e632c08bda92dd001c
rayhan <aburayhandh@gmail.com>
2023-01-12 22:21:42 +0600

    profile card social function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 /*=============== SHOW SOCIAL NETWORKS ===============*/
+const showSocial = (toggleCard, socialCard) => {
+    const toggle = document.getElementById(toggleCard),
+        social = document.getElementById(socialCard)
+
+    toggle.addEventListener('click', () => {
+        social.classList.toggle('animation');
+    })
+}
+
+showSocial('card-toggle', 'card-social');
